82ccf183564ac8c185ec1d5747792ce652a2d834
Water-Stone <wngjsthd8@naver.com>
2023-11-23 03:04:39 +0900

    migrate

--- accounts/models.py
+++ accounts/models.py
@@ -6,5 +6,25 @@ class CustomUser(AbstractUser):
     bio = models.TextField(max_length=500, blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
 
+    # Resolve reverse accessor clash for groups
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',
+        related_query_name='customuser',
+        blank=True,
+        verbose_name='groups',
+        help_text='The groups this user belongs to.',
+    )
+
+    # Resolve reverse accessor clash for user_permissions
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_set',
+        related_query_name='customuser',
+        blank=True,
+        verbose_name='user permissions',
+        help_text='Specific permissions for this user.',
+    )
+
     def __str__(self):
         return self.username
